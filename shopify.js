83c99d19eb56704e0c5c7791f9c0053780a762f2
Steven Pichardo <stevepichardo@outlook.com>
2019-01-21 02:04:47 -0500

    cleaned up code more..took out some hard coded values

--- shopify.js
+++ shopify.js
@@ -1,8 +1,12 @@
 const request = require('request-promise');
 
-const getProducts = async () => {
+// Async Function - Sends a request to shopify site passed in
+const getProducts = async uri => {
+  const productArr = [];
+  const itemArr = [];
+
   const options = {
-    uri: 'https://cncpts.com/products.json',
+    uri: `${uri}/products.json`,
     headers: {
       Accept:
         'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
@@ -10,23 +14,22 @@ const getProducts = async () => {
       'Cache-Control': 'max-age=0',
       gzip: false,
       Connection: 'keep-alive',
-      Host: 'cncpts.com',
       'Upgrade-Insecure-Requests': 1,
       'User-Agent':
         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
     }
   };
 
-  const productArr = [];
-  const itemArr = [];
-
+  // Response holds one object Products with multiple product in it
   let response = JSON.parse(await request(options));
   response = response.products;
 
+  // Getting variants for each product
   for (var i = 0; i < response.length; i++) {
     productArr.push(response[i].variants);
   }
 
+  // Looping through variants properties then adding to itemArr
   for (var i = 0; i < productArr.length; i++) {
     for (var j = 0; j < productArr[i].length; j++) {
       const item = productArr[i][j];
@@ -42,4 +45,4 @@ const getProducts = async () => {
   console.log(itemArr);
 };
 
-getProducts();
+getProducts('https://undefeated.com');
